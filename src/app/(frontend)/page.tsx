7c14d4bcf91143f3f1feec5468b9a7f36d4cf140
tkyip3 <86195011+tkyip3@users.noreply.github.com>
2026-01-23 14:27:26 +0800

    index layout

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import Tilt from '@/components/Tilt'
 import './styles.css'
 export default async function HomePage() {
   return (
     <>
       <div className="home-banner">
-        <div className="container mx-auto">
-          <div className="flex">
-            <div className="w-1/2">1</div>
-            <div className="w-1/2">
-              <div className="banner-title bg-red-500">祝福手繪</div>
-              <div className="banner-subtitle">手繪</div>
+        <div className="banner-wrapper">
+          <div className="w-1/2">
+            <div className="banner-card-wrapper">
+              <Tilt
+                className="banner-card"
+                options={{ max: 25, speed: 1000, glare: true, 'max-glare': 0.5, scale: 1.05 }}
+              >
+                <div className="card-title">demo</div>
+              </Tilt>
             </div>
+          </div>
+          <div className="w-1/2">
+            <div className="banner-title">祝福手繪</div>
+            <div className="banner-subtitle">手繪</div>
           </div>
         </div>
       </div>
---
+++ src/components/Tilt.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import React, { useRef, useEffect, ReactNode } from 'react'
+import VanillaTilt from 'vanilla-tilt'
+
+const Tilt = ({
+  children,
+  options = {},
+  className,
+}: {
+  children: ReactNode
+  options?: Record<string, unknown>
+  className?: string
+}) => {
+  const tiltRef = useRef(null)
+
+  useEffect(() => {
+    const { current: tiltNode } = tiltRef
+    if (tiltNode) {
+      VanillaTilt.init(tiltNode, options)
+    }
+  }, [options])
+
+  return (
+    <div className={className} ref={tiltRef}>
+      {children}
+    </div>
+  )
+}
+
+export default Tilt
