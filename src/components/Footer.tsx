d6bb539a758766fcc1fbd2a67f0282fe345b8218
tkyip3 <86195011+tkyip3@users.noreply.github.com>
2026-04-22 10:40:04 +0800

    footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,26 @@
+import { Icon } from '@iconify/react'
+import Link from 'next/link'
 export default function Footer() {
   return (
     <footer>
       <div className="container mx-auto text-center p-4">
-        © {new Date().getFullYear()} YipTsz Blessive Brush All rights reserved
+        <div className="footer-top">
+          <div className="footer-menu">
+            <Link
+              href={'https://www.instagram.com/yiptsz.blessive.brush/'}
+              target="_blank"
+              className="menu-item"
+            >
+              <Icon className="" icon="arcticons:instagram" width="2em" height="2em" />
+            </Link>
+            <Link href={'https://wa.me/85295895746?text='} target="_blank" className="menu-item">
+              <Icon className="" icon="arcticons:whatsapp" width="2em" height="2em" />
+            </Link>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          © {new Date().getFullYear()} YipTsz Blessive Brush All rights reserved
+        </div>
       </div>
     </footer>
   )
