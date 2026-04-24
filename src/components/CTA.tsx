461a4911bcb826dd95f6f4fb3b81a9cadc6ff3f8
tkyip3 <86195011+tkyip3@users.noreply.github.com>
2026-04-24 11:51:16 +0800

    cta layout update

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -5,10 +5,18 @@ export default function CTA() {
       <div className="CTA-title">對「祝福手繪」有興趣？歡迎了解更多！</div>
       <div className="CTA-list">
         <Link href="/events/332a2631-c809-41ae-8e59-7ae5f33f511b" className="CTA-item">
-          體驗班
+          <div className="item-bg"></div>
+          <div className="item-main">
+            <div className="main-icon">🎨</div>
+            <div className="main-title">體驗班</div>
+          </div>
         </Link>
         <Link href="/events/332a2631-c809-41ae-8e59-7ae5f33f511b" className="CTA-item">
-          初級導師課程
+          <div className="item-bg"></div>
+          <div className="item-main">
+            <div className="main-icon">👩‍🏫</div>
+            <div className="main-title">初級導師課程</div>
+          </div>
         </Link>
       </div>
     </div>
