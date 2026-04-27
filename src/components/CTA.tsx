e85b3c68db314bfae0e99d7638c4ed2a226efbcd
tkyip3 <86195011+tkyip3@users.noreply.github.com>
2026-04-27 09:57:34 +0800

    home page layout fix

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -9,13 +9,15 @@ export default function CTA() {
           <div className="item-main">
             <div className="main-icon">🎨</div>
             <div className="main-title">體驗班</div>
+            <div className="main-detail">無需畫畫經驗，輕鬆體驗手繪字的樂趣。</div>
           </div>
         </Link>
-        <Link href="/events/332a2631-c809-41ae-8e59-7ae5f33f511b" className="CTA-item">
+        <Link href="/events/1f0c09f8-f909-43f8-970d-754bce1570ad" className="CTA-item">
           <div className="item-bg"></div>
           <div className="item-main">
             <div className="main-icon">👩‍🏫</div>
             <div className="main-title">初級導師課程</div>
+            <div className="main-detail">進深教學，掌握更多手繪字的技巧。</div>
           </div>
         </Link>
       </div>
